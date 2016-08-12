70cd0bc5e1603341928f894a6401f91688770c61
Franck Cotton <franck.cotton@insee.fr>
2016-08-12 15:22:33 +0200

    Fixed classification query

--- src/js/utils/sparql-queries.js
+++ src/js/utils/sparql-queries.js
@@ -1,7 +1,7 @@
 /**
  * Builds the query that retrieve the list of all classifications.
  */
-const buildGetClassificationListQuery = `
+const buildGetClassificationListQuery = () => `
   PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
   PREFIX skos:<http://www.w3.org/2004/02/skos/core#>
   SELECT ?classification ?code ?label WHERE {
@@ -12,7 +12,7 @@ const buildGetClassificationListQuery = `
 /**
  * Builds the query that gets the details about a classification.
  */
-const buildGetClassificationDetailsQuery = uri =>  `
+const buildGetClassificationDetailsQuery = uri => `
   PREFIX dcterms: <http://purl.org/dc/terms/>
   PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
   SELECT ?code ?label ?issued WHERE {
@@ -24,7 +24,7 @@ const buildGetClassificationDetailsQuery = uri =>  `
  * Builds the query that gets the levels of a classification.
  * Using SPARQL 1.1 property paths, see https://www.w3.org/TR/sparql11-query/#propertypaths
  */
-const buildGetClassificationLevelsQuery = uri =>  `
+const buildGetClassificationLevelsQuery = uri => `
   PREFIX rdf:<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
   PREFIX skos:<http://www.w3.org/2004/02/skos/core#>
   PREFIX xkos:<http://rdf-vocabulary.ddialliance.org/xkos#>
