f1b5e6670f70a3bb9457b65913c95b5748255021
Olivier Levitt <ungawa14@gmail.com>
2016-08-12 12:17:27 +0200

    Classification details refactoring

--- src/js/components/classification-details.js
+++ src/js/components/classification-details.js
@@ -2,6 +2,7 @@
 import React, { Component } from 'react'
 import { loadClassificationItemsIfNeeded } from '../actions/classification'
 import { connect } from 'react-redux'
+import ClassificationItemList from './classification-item-list'
 
 
 
@@ -13,40 +14,18 @@ class ClassificationDetails extends Component {
   }
 
   componentWillMount() {
-    this.props.loadClassificationItemsIfNeeded(this.props.uri)
+
   }
 
   render() {
-    const { loaded, items } = this.props
-    if (!loaded) return <span>loading items</span>
-    return (
-      <ul>
-        {
-          items.map(item =>
-            <li key={item}>
-                {item}
-            </li>
-          )
-        }
-      </ul>
-    )
+    return <ClassificationItemList uri={this.props.uri}/>
   }
 }
 
-const mapStateToProps = (state, { uri }) => {
-  if (!state.classificationItems.hasOwnProperty(uri)) return {
-    loaded: false,
-    items: []
-  }
-  const items = state.classificationItems[uri]
-  return {
-    loaded: items.loaded,
-    items: items.items
-  }
-}
+const mapStateToProps = state => ({
+})
 
 const mapDispatchToProps = {
-  loadClassificationItemsIfNeeded
 }
 
-export default connect(mapStateToProps, mapDispatchToProps)(ClassificationItems)
+export default connect(mapStateToProps, mapDispatchToProps)(ClassificationDetails)
---
+++ src/js/components/classification-item-list.js
@@ -0,0 +1,52 @@
+
+import React, { Component } from 'react'
+import { loadClassificationItemsIfNeeded } from '../actions/classification'
+import { connect } from 'react-redux'
+
+
+
+
+class ClassificationItemList extends Component {
+
+  constructor(props) {
+    super(props)
+  }
+
+  componentWillMount() {
+    this.props.loadClassificationItemsIfNeeded(this.props.uri)
+  }
+
+  render() {
+    const { loaded, items } = this.props
+    if (!loaded) return <span>loading items</span>
+    return (
+      <ul>
+        {
+          items.map(item =>
+            <li key={item}>
+                {item}
+            </li>
+          )
+        }
+      </ul>
+    )
+  }
+}
+
+const mapStateToProps = (state, { uri }) => {
+  if (!state.classificationItems.hasOwnProperty(uri)) return {
+    loaded: false,
+    items: []
+  }
+  const items = state.classificationItems[uri]
+  return {
+    loaded: items.loaded,
+    items: items.items
+  }
+}
+
+const mapDispatchToProps = {
+  loadClassificationItemsIfNeeded
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(ClassificationItemList)
