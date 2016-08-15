4fc6ef6ba81a9a775fe73a4d0fd071730b72529a
Jean-Baptiste RUDANT <jb@noknot.fr>
2016-08-15 11:45:36 +0200

    Use babel polyfill (fix #5)

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,8 @@
+import 'babel-polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from './components/root'
 require('file?name=[name].[ext]!../index.html');
-
-//TODO replace with babel polyfill
-if (!String.prototype.includes) {
-  String.prototype.includes = function(search, start) {
-    'use strict';
-    if (typeof start !== 'number') {
-      start = 0;
-    }
-
-    if (start + search.length > this.length) {
-      return false;
-    } else {
-      return this.indexOf(search, start) !== -1;
-    }
-  };
-}
 
 ReactDOM.render(
 	<Root/>,
