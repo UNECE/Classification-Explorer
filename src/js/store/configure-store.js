6503a2d3cda3bbce1a393e448ad907fdf20478ad
Jean-Baptiste RUDANT <jb@noknot.fr>
2016-08-15 11:43:15 +0200

    Import explicitly `index.js`

--- src/js/store/configure-store.js
+++ src/js/store/configure-store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware, compose } from 'redux'
 import thunkMiddleware from 'redux-thunk'
 import createLogger from 'redux-logger'
-import mainReducer from '../reducers'
+import mainReducer from '../reducers/index'
 
 const loggerMiddleware = createLogger()
 
