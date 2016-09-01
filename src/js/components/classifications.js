1c7a13a4de1f5b97efd2e7567a66ca1418e3a027
Jean-Baptiste RUDANT <jb@noknot.fr>
2016-09-01 17:58:08 +0200

    Remove unnecessary code since the introduction of the router

--- src/js/components/classifications.js
+++ src/js/components/classifications.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import { switchViewClassificationDetails } from '../actions/app-state'
 import { sparqlConnect } from '../sparql/configure-sparql'
 import { LOADING, LOADED, FAILED } from 'sparql-connect'
 import { connect } from 'react-redux'
@@ -7,16 +6,9 @@ import { Link } from 'react-router'
 import Loading from './loading.js'
 import { URIToRoute } from '../router-mapping'
 
-function Classifications({ loaded, classifications,
-    switchViewClassificationDetails }) {
-  if (loaded !== LOADED) {
-    return( <Loading from="Classification" plural={false} />
-          )
-           } else {
-  //we could also write something like
-  //if (loaded === FAILED)
-  //  return <span>error while retrieving clasifications/span>
-  
+function Classifications({ loaded, classifications }) {
+  if (loaded !== LOADED) return  <Loading from="Classification" plural={false} />
+
   //`URIToRoute.classification(...)` will return a string like `nacer2/nace`
   //where `nacer2` is the `classification` paremeter for the router, and `nace`
   //the `conceptScheme` paremeter
@@ -35,11 +27,5 @@ function Classifications({ loaded, classifications,
     </div>
   )
 }
-}
 
-const mapDispatchToProps = {
-  switchViewClassificationDetails
-}
-
-export default connect(undefined, mapDispatchToProps)(
-  sparqlConnect.classifications(Classifications))
+export default sparqlConnect.classifications(Classifications)
