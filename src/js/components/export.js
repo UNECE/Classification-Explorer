1a2f675a59c27dc089527fbcab4be7284d211006
Olivier Levitt <ungawa14@gmail.com>
2016-09-13 14:57:39 +0200

    Export button should not use the router

--- src/js/components/export.js
+++ src/js/components/export.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router'
 
 export default function Export({dataToExport, name}){
   return (
-    <Link to="export"><button className="fa fa-download" onClick={() => handleClick(dataToExport,name)}></button></Link>
+    <button className="fa fa-download" onClick={() => handleClick(dataToExport,name)}></button>
   )
 }
 
