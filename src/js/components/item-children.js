560fca975d2bb6fb221958bb12bd90d4dc9503f3
Enrico80 <eorsini@istat.it>
2016-09-13 11:55:28 +0200

    Added export button on every list <Fix #28>

--- src/js/components/item-children.js
+++ src/js/components/item-children.js
@@ -4,10 +4,13 @@ import { LOADING, LOADED, FAILED } from 'sparql-connect'
 import Loading from './loading'
 import { Link } from 'react-router'
 import { uriToLink } from '../router-mapping'
+import Export from './export.js'
 
 function ItemChildren({ item, loaded, items }) {
   if (loaded !== LOADED) return  <Loading from="Item children" plural={false} />
   return (
+      <span>
+      <Export dataToExport={items} name="export" />
     <ul>
       { items.map(({ item, code, label }) =>
         <li key={item}>
@@ -16,6 +19,7 @@ function ItemChildren({ item, loaded, items }) {
           </Link>
         </li>) }
     </ul>
-  ) 
+    </span>
+  )
 }
 export default sparqlConnect.itemChildren(ItemChildren)
