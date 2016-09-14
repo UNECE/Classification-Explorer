5d23de27ab77ebdb23db387b7c8e906aaddec71a
Eric Debonnel <eric.debonnel@casd.eu>
2016-09-14 10:23:29 +0200

    Filtered results in english

--- src/js/components/item-result.js
+++ src/js/components/item-result.js
@@ -2,10 +2,10 @@ import React from 'react'
 import { Link } from 'react-router'
 import { uriToLink } from '../router-mapping'
 
-export default function ItemResult({ item, itemLabel, predicate, match, code }) {
+export default function ItemResult({ item, itemLabel, predicate, match, code, coreContentNoteText, additionalContentNoteText }) {
   return (
     <Link to={uriToLink.itemDetails(item)}>
-    {code}  -  { itemLabel }
+    {code}  -  { itemLabel } - (NOTES :  {coreContentNoteText} ; {additionalContentNoteText})
     </Link>
   )
 }
