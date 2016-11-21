a6da75b799050f8229d022e590c22c7f9a6e98fc
Jean-Baptiste RUDANT <jb@noknot.fr>
2016-11-21 22:14:15 +0100

    Clean code

--- src/js/config.js
+++ src/js/config.js
@@ -1,3 +1,5 @@
 // Parameters for the application
 
-export default {queryURL: 'https://unece-stardog.ichec.ie/annex/classifications/sparql/query'}
+export default {
+  queryURL: 'https://unece-stardog.ichec.ie/annex/classifications/sparql/query'
+}
