70a5709ebf1f13c85ff1fcefe191f49d840963e5
Franck Cotton <franck.cotton@insee.fr>
2018-11-16 10:54:03 +0100

    Change backend to LOS GraphDB

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 // Parameters for the application
 
 export default {
-  queryURL: 'https://unece-stardog.ichec.ie/annex/classifications/sparql/query'
+  queryURL: 'http://graphdb.linked-open-statistics.org/repositories/classifications'
 }
